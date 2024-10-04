3f8c6acc523e2a2ea723c011b301dd42b718401d
Dorannium <dorianshuu@gmail.com>
2024-10-04 10:14:32 -0400

    "Enter Value" has been added.

--- Chapter2Ex18_Cody_Drumheller.cpp
+++ Chapter2Ex18_Cody_Drumheller.cpp
@@ -19,14 +19,15 @@ that the user can enter the number of the number of customers that were surveyed
 using namespace std;
 int main()
 {
-    const int total_customers = 16500;
+    int total_customers;
     const double energy_drink_purchasers_percent = 0.15;
-	const double citrus_preference_percent = 0.58;
+    const double citrus_preference_percent = 0.58;
+	cout << "Enter the total number of customers surveyed: ";
+	cin >> total_customers;
 	int energy_drink_customers = total_customers * energy_drink_purchasers_percent;
 	int citrus_customers = energy_drink_customers * citrus_preference_percent;
-	cout << "Approximate number of customers who purchase one or more energy drinks per week: " << energy_drink_customers << endl;
+	cout << "Approximate number if customers who purchase one or more energy drinks per week: " << energy_drink_customers << endl;
 	cout << "Approximate number of customers who prefer citrus-flavored energy drinks: " << citrus_customers << endl;
 	return 0;
-
 }
 
